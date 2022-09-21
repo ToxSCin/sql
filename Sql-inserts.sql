d97891f49e0475cee0d9ceeb96dfe0e1bff036d1
Renato (Tox) <102253624+ToxSCin@users.noreply.github.com>
2022-09-21 14:14:18 -0300

    trigger

---
+++ Sql-inserts.sql
@@ -0,0 +1,17 @@
+insert into produto (descricao, preco ,estoque)
+values ("RTX 360", "2600","2");
+
+insert into produto (descricao, preco,estoque)
+values ("RTX 120", "2500","2");
+
+insert into produto (descricao, preco,estoque)
+values ("RTX 142", "2400","2");
+
+insert into produto (descricao, preco,estoque)
+values ("RTX 530", "2300","2");
+
+insert into produto (descricao, preco,estoque)
+values ("RTX 631", "2200","2");
+
+insert into produto (descricao, preco,estoque)	
+values ("RTX 400", "2100","2");
